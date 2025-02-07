a7a8f308c9e3c213050c55b236bcf04f96474d7a
Harsh10092000 <143779029+Harsh10092000@users.noreply.github.com>
2025-02-07 15:20:35 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env: {
-        // webURL: "https://api.propertyease.in/",
-        webURL: "http://localhost:8010/",
+        webURL: "https://api.propertyease.in/",
+        //webURL: "http://localhost:8010/",
       },
 };
 
